8b4a827f5279a40323da13c8816ecde87fc47122
devAlex.ᴱᵀᴴ <88956822+devAlexDotEth@users.noreply.github.com>
2025-12-09 15:31:10 -0500

    fix: React 19 ref callback type error

--- components/EmailVerification.tsx
+++ components/EmailVerification.tsx
@@ -143,7 +143,7 @@ export const EmailVerification: React.FC<EmailVerificationProps> = ({
             {otp.map((digit, index) => (
               <input
                 key={index}
-                ref={el => inputRefs.current[index] = el}
+                ref={el => { inputRefs.current[index] = el; }}
                 type="text"
                 inputMode="numeric"
                 maxLength={1}
